942e0ecaff9b9466d21d5310a6de6247b7ec9e75
kinkey232 <meerr67@gmail.com>
2021-07-19 12:10:34 +0300

    backend

---
+++ backend/src/main/java/com/example/login2/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.login2.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/example/login2/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.example.login2.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
